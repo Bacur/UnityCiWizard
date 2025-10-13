d5bca5997d2fea63f62cd3fe7c6171de607708af
Stepan Selyuchenko <sselyuchenko@mail.ru>
2025-10-13 16:12:28 +0700

    add local build server cache and grouped logs

--- Editor/BuildLogHandler.cs
+++ Editor/BuildLogHandler.cs
@@ -1,19 +1,26 @@
 using System;
 using UnityEngine;
 
-namespace CiWizard.Editor {
-    public class BuildLogHandler : ILogHandler{
+namespace CiWizard.Editor
+{
+    public class BuildLogHandler : ILogHandler
+    {
         private static readonly ILogHandler UnityLogHandler = Debug.unityLogger.logHandler;
 
-        public void LogException(Exception exception, UnityEngine.Object context) {
+        private int count = 0;
+
+        public void LogException(Exception exception, UnityEngine.Object context)
+        {
             SetColor(31);
             UnityLogHandler.LogException(exception, context);
             SetColor(0);
         }
 
-        public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args) {
+        public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+        {
             int colorCode;
-            switch (logType) {
+            switch (logType)
+            {
                 case LogType.Error:
                     colorCode = 31;
                     break;
@@ -33,15 +40,17 @@ namespace CiWizard.Editor {
                     colorCode = 0;
                     break;
             }
+
             SetColor(colorCode);
             UnityLogHandler.LogFormat(logType, context, format, args);
             SetColor(0);
         }
 
-        public static void WriteSectionBegin(string sectionName, string sectionHeader) {
+        public static void WriteSectionBegin(string sectionName, string sectionHeader, bool isCollapsed = false)
+        {
             SetColor(96);
             Console.WriteLine((char)27);
-            Console.Write($"[0Ksection_start:{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}:{sectionName}");
+            Console.Write($"[0Ksection_start:{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}:{sectionName}[collapsed={isCollapsed}]");
             Console.Write((char)13);
             Console.Write((char)27);
             Console.Write("[0K");
@@ -49,8 +58,9 @@ namespace CiWizard.Editor {
             Console.WriteLine();
             SetColor(0);
         }
-        
-        public static void WriteSectionEnd(string sectionName) {
+
+        public static void WriteSectionEnd(string sectionName)
+        {
             SetColor(96);
             Console.WriteLine((char)27);
             Console.Write($"[0Ksection_end:{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}:{sectionName}");
@@ -61,7 +71,8 @@ namespace CiWizard.Editor {
             SetColor(0);
         }
 
-        private static void SetColor(int colorCode) {
+        private static void SetColor(int colorCode)
+        {
             Console.Write((char)27);
             Console.Write($"[{colorCode}m");
         }
